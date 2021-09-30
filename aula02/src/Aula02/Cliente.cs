00c51bc56309f99d3f0f198bdf2063fca2b91db6
Carlos Machel <carlostiago@outlook.com.br>
2021-09-30 18:54:36 -0300

    Aula 02

---
+++ aula02/src/Aula02/Cliente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Aula02
+{
+    public class Cliente
+    {
+        public int Idade { get; private set; }
+        public string Nome { get; private set; }
+
+        public Cliente(int idade, string nome)
+        {
+            if (!EhMaiorDeIdade(idade)) throw new Exception();
+
+            Idade = idade;
+            Nome = nome;
+
+            //passo1
+            //passo2
+
+            //passo3
+            //passo4
+
+        }
+
+        private bool EhMaiorDeIdade(int idade)
+        {
+            return idade > 18;
+        }
+    }
+
+    public interface IClienteValidator
+    {
+        bool EhMaiorDeIdade(int idade);
+    }
+
+    public class ClienteValidator : IClienteValidator
+    {
+        public bool EhMaiorDeIdade(int idade)
+        {
+            return idade > 18;
+        }
+    }
+}
---
+++ aula02/src/Aula02/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula02
+{
+    public class Produto
+    {
+        public DateTime? DataValidade { get; set; }
+
+        public bool EstaValido(DateTime data)
+        {
+            return DataValidade >= data;
+        }
+    }
+}
