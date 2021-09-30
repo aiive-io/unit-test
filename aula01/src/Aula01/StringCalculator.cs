b28861c634b0b59c8d593d94e8e0b016097f1914
Carlos Machel <carlostiago@outlook.com.br>
2021-09-30 18:46:17 -0300

    Aula 01

---
+++ aula01/src/Aula01/StringCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace Aula01
+{
+    public class StringCalculator
+    {
+        public int Add(string number)
+        {
+            if (string.IsNullOrWhiteSpace(number))
+                return 0;
+
+            var numbers = number.Split(",").Select(x => int.Parse(x));
+            return numbers.Sum();
+        }
+    }
+}
---
+++ aula01/test/Aula01.UnitTest/StringCalculatorTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Aula01.UnitTest
+{
+    public class StringCalculatorTests
+    {
+        [Fact]
+        public void Add_ParametroVazio_Retorna0()
+        {
+            //Arrange
+            var calculator = CreateStringCalculator();
+
+            //Act
+            var result = calculator.Add("");
+
+            //Assert
+            result.Should().Be(0);
+            Assert.Equal(0, result);
+
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("2", 2)]
+        public void Add_ParametroComUmNumero_RetornaNumero(string numbers, int esperado)
+        {
+            //Arrange
+            var calculator = CreateStringCalculator();
+
+            //Act
+            var result = calculator.Add(numbers);
+
+            //Assert
+            result.Should().Be(esperado);
+            Assert.Equal(esperado, result);
+        }
+
+
+        [Theory]
+        [InlineData("1,2", 3)]
+        [InlineData("1,3", 4)]
+        public void Add_ParametroComDoisNumeros_RetornaSoma(string numeros, int esperado)
+        {
+            //Arrange
+            var calculator = CreateStringCalculator();
+
+            //Act
+            var result = calculator.Add(numeros);
+
+            //Assert
+            result.Should().Be(esperado);
+            Assert.Equal(esperado, result);
+        }
+
+        [Fact]
+        public void Add_ParametrosComMaisDeDoisNumeros_RetornaSoma()
+        {
+            var calculator = CreateStringCalculator();
+
+            var result = calculator.Add("1,2,3");
+
+            result.Should().Be(6);
+        }
+
+        private StringCalculator CreateStringCalculator()
+        {
+            return new StringCalculator();
+        }
+    }
+}
