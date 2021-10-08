067b2b6912b84612f35bb62f961eac2fd722bd85
Carlos Machel <carlostiago@outlook.com.br>
2021-10-08 10:54:00 -0300

    Criacao do ItemBuilder e Testes do GildedRose

---
+++ aula05/test/Aula05.GildedRose.Tests.Shared/ItemBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula05.GildedRose.UnitTests
+{
+    public class ItemBuilder
+    {
+        protected Item _item;
+
+        public ItemBuilder() 
+        {
+            _item = new Item();
+        }
+
+        public ItemBuilder ComNome(string name)
+        {
+            _item.Name = name;
+            return this;
+        }
+
+        public ItemBuilder ComQualidade(int qualidade)
+        {
+            _item.Quality = qualidade;
+            return this;
+        }
+
+        public ItemBuilder ComSellIn(int sellIn)
+        {
+            _item.SellIn = sellIn;
+            return this;
+        }
+
+        public Item Build()
+        {
+            return _item;
+        }
+    }
+
+    public class SulfurasBuilder : ItemBuilder
+    {
+        public SulfurasBuilder(int sellIn) :base()
+        {
+            this.ComNome("Sulfuras, Hand of Ragnaros")
+                .ComQualidade(80)
+                .ComSellIn(sellIn);             
+        }
+    }
+}
--- aula05/test/Aula05.GildedRose.UnitTests/GildedRoseTests.cs
+++ aula05/test/Aula05.GildedRose.UnitTests/GildedRoseTests.cs
@@ -10,16 +10,29 @@ namespace Aula05.GildedRose.UnitTests
 {
     public class GildedRoseTests
     {
-        [Fact]
-        public void UpdateQuality_ItemSulfuras_NuncaAlteraSellIn()
+        private Item CriarSulfuras(int quality, int sellin)
         {
-            //arrange
             var item = new Item
             {
                 Name = "Sulfuras, Hand of Ragnaros",
-                Quality = 80,
-                SellIn = -1
+                Quality = quality,
+                SellIn = sellin
             };
+
+            return item;
+        }
+
+        private GildedRose CriarGildRose(Item item)
+        {
+            return new GildedRose(new List<Item> { item });
+        }
+
+        [Fact]
+        [Trait("UpdateQuality", "Sulfuras")]
+        public void UpdateQuality_ItemSulfuras_NuncaAlteraSellIn()
+        {
+            //arrange
+            var item = new SulfurasBuilder(-1).Build();                
 
             var items = new List<Item>() { item };
             var gildedRose = new GildedRose(items);
@@ -31,17 +44,13 @@ namespace Aula05.GildedRose.UnitTests
             item.SellIn.Should().Be(-1);
         }
 
-
         [Fact]
+        [Trait("UpdateQuality", "Sulfuras")]
         public void UpdateQuality_ItemSulfuras_NuncaCaiQualidade()
         {
             //arrange
-            var item = new Item
-            {
-                Name = "Sulfuras, Hand of Ragnaros",
-                Quality = 80,
-                SellIn = 0
-            };
+            var item = CriarSulfuras(80, -1);
+
             var items = new List<Item>() { item };
             var gildedRose = new GildedRose(items);
 
@@ -50,6 +59,87 @@ namespace Aula05.GildedRose.UnitTests
 
             //assert
             item.Quality.Should().Be(80);
+        }
+
+        [Theory]
+        [InlineData(2,-1,4,-2)]
+        [InlineData(3, -2, 5, -3)]      
+        [InlineData(50, -1, 50, -2)]
+        public void UpdateQuality_ItemAgedBrieSellInMenorQueZero_QualidadeSobeDuasUnidadesMaximo50(int quality,
+            int sellIn,
+            int qualityEsperado,
+            int sellInEsperado)
+        {
+            //arrange
+            var item = new Item
+            {
+                Name = "Aged Brie",
+                Quality = quality,
+                SellIn = sellIn
+            };
+
+            var gildedRose = CriarGildRose(item);
+
+            //act
+            gildedRose.UpdateQuality();
+
+            //assert
+            item.Quality.Should().Be(qualityEsperado);
+            item.SellIn.Should().Be(sellInEsperado);
+        }
+
+        [Theory]
+        [InlineData(2, 2, 3, 1)]
+        [InlineData(2, 1, 3, 0)]
+        [InlineData(50, 1, 50, 0)]
+        public void UpdateQuality_ItemAgedBrieSellInMaiorOuIgualAZero_QualidadeSobeUmaUnidadeMaximo50(
+            int quality, 
+            int sellIn, 
+            int qualityEsperado,
+            int sellInEsperado)
+        {
+            //arrange
+            var item = new Item
+            {
+                Name = "Aged Brie",
+                Quality = quality,
+                SellIn = sellIn
+            };
+
+            var gildedRose = CriarGildRose(item);
+
+            //act
+            gildedRose.UpdateQuality();
+
+            //assert
+            item.Quality.Should().Be(qualityEsperado);
+            item.SellIn.Should().Be(sellInEsperado);
+        }
+
+        [Theory]
+        [InlineData(2,11,3,10)]
+        public void UpdateQuality_ItemBackstageSellInMaiorQue10_QualidadeSobeUmaUnidade(
+            int quality,
+            int sellIn,
+            int qualityEsperado,
+            int sellInEsperado)
+        {
+            //arrange
+            var item = new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                Quality = quality,
+                SellIn = sellIn
+            };
+
+            var gildedRose = CriarGildRose(item);
+
+            //act
+            gildedRose.UpdateQuality();
+
+            //assert
+            item.Quality.Should().Be(qualityEsperado);
+            item.SellIn.Should().Be(sellInEsperado);
         }
     }
 }
